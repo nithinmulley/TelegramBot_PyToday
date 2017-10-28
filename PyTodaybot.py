dea245bafdbf02e99ad7e4db9199288d0fe77389
Nithin Mulley <31166151+nithinmulley@users.noreply.github.com>
2017-10-28 20:13:52 +0530

    Code Cleaned

--- PyTodaybot.py
+++ PyTodaybot.py
@@ -5,10 +5,17 @@ from datetime import datetime, timedelta
 from pytz import timezone
 from pytz import utc
 import re
+from os.path import expanduser
 
-token = '425349659:AAHF19iJ1hvVC_ihXQo2TAAPgLOY6biTjAU'
-url = 'https://api.telegram.org/bot{token}/'.format(**locals())
-temp_user_list = []
+
+# Store and your Bot token in your home directory -> ~/.tokens/telegram_bot
+home = expanduser("~")
+token_file = str(home) + '/.tokens/telegram_bot'
+with open(token_file) as f:
+    token = f.readline().strip()
+
+
+tg_url = 'https://api.telegram.org/bot{token}/'.format(**locals())
 
 
 def get_url(url):
@@ -16,20 +23,18 @@ def get_url(url):
     return json.loads(response.text)
 
 
+def get_updates(offset=None):
+    if offset:
+        return get_url(tg_url+'getUpdates?timeout=100'+"&offset={offset}".format(**locals()))
+    return get_url(tg_url+'getUpdates?timeout=100')
+
+
 def send_updates(chat_id, text):
-    return get_url(url+'sendMessage?chat_id={chat_id}&text={text}'.format(**locals()))
+    return get_url(tg_url+'sendMessage?chat_id={chat_id}&text={text}'.format(**locals()))
 
 
 def send_inline(answer):
-    return requests.post(url=r'https://api.telegram.org/bot425349659:AAHF19iJ1hvVC_ihXQo2TAAPgLOY6biTjAU'
-                              '/answerInlineQuery',
-                         params=answer)
-
-
-def get_updates(offset=None):
-    if offset:
-        return get_url(url+'getUpdates?timeout=100'+"&offset={offset}".format(**locals()))
-    return get_url(url+'getUpdates?timeout=100')
+    return requests.post(url=tg_url + 'answerInlineQuery', params=answer)
 
 
 def last_update_id(updates):
@@ -47,7 +52,7 @@ def echo_all(updates):
             from_id = update['message']['from']['username']
             # print('Received from ' + from_id + ' > ' + text)
             send_updates(chat_id, text)
-            print('Replied to ' + from_id + ' > ' + text)
+            print('Chat Echoed ' + from_id + ' > ' + text)
         except Exception as e:
             print(e)
 
@@ -103,38 +108,60 @@ def get_inline_hydpy_meetup():
     return meetup_details
 
 
+def prt_recd_from(data, method, query):
+    try:
+        from_id = data['from']['username']
+        if method == 'inline_query':
+            print('Inline Query Received from ' + from_id + ' > ' + query)
+        elif method == 'message':
+            print('Chat Received from ' + from_id + ' > ' + query)
+        return from_id
+    except Exception as e:
+        print(e)
+    return
+
+
+def prt_sent_to(from_id, method, action):
+    try:
+        if from_id:
+            if method == 'inline_query':
+                print('Inline Query answered to ' + from_id + ' > ' + action)
+            elif method == 'message':
+                print('Chat Replied to ' + from_id + ' > ' + action)
+    except Exception as e:
+        print(e)
+    return
+
+
 def commander(updates):
     for update in updates['result']:
         if 'inline_query' in update:
             inline_query = update['inline_query']
             inline_query_id = inline_query['id']
             query = inline_query['query']
-            try:
-                from_id = inline_query['from']['username']
-                print('Query Received from ' + from_id + ' > ' + query)
-            except Exception as e:
-                print(e)
+            from_id = prt_recd_from(inline_query, 'inline_query', query)
             if re.search('hydpy', query, re.IGNORECASE):
                 meetup_list = get_inline_hydpy_meetup()
                 results = []
-                for j, i in enumerate(meetup_list):
+                for u_id, meetup in enumerate(meetup_list):
                     results.append({'type': 'article',
-                                    'id': j,
-                                    'title': i['meetname'],
+                                    'id': u_id,
+                                    'title': meetup['meetname'],
                                     'parse_mode': 'Markdown',
-                                    'message_text': i['text'],
-                                    'description': i['url']})
+                                    'message_text': meetup['text'],
+                                    'description': meetup['url']})
                 answer = {'inline_query_id': inline_query_id, 'results': json.dumps(results), 'cache_time': '30'}
                 send_inline(answer)
+                prt_sent_to(from_id, 'inline_query', 'HydPy Meetup Details')
         else:
             try:
-                chat_id = update['message']['chat']['id']
-                text = update['message']['text']
-                from_id = update['message']['from']['username']
-                print('Received from ' + from_id + ' > ' + text)
-                if re.search('hydpy', text, re.IGNORECASE):
+                message = update['message']
+                chat_id = message['chat']['id']
+                query = message['text']
+                from_id = prt_recd_from(message, 'message', query)
+                if re.search('hydpy', query, re.IGNORECASE):
                     get_hydpy_meetup(chat_id)
-                    print('Replied to ' + from_id + ' > ' + 'Meetup Details')
+                    prt_sent_to(from_id, 'message', 'HydPy Meetup Details')
                 else:
                     echo_all(updates)
             except Exception as e:
